800efd448868176dcfa31168584b321ada58dcf2
pytergeist <tompopeworks@gmail.com>
2024-08-16 09:27:41 +0100

    Adding simple calculate_pair_frequency method

--- src/tokenisers/byte_pair_encoding.py
+++ src/tokenisers/byte_pair_encoding.py
@@ -1,16 +1,31 @@
-from collections import Counter
+import numpy
+from collections import Counter, defaultdict
 
 
 class BytePairEncoding:
     def __init__(self):
         pass
 
-    def get_word_frequencies(self, corpus):
+    def get_vocab_frequencies(self, corpus):
         word_frequencies = Counter(corpus.split())
         return {word: frequency for word, frequency in word_frequencies.items()}
+
+    def split_vocab_into_chars(self, vocab):
+        return {" ".join(key): value for key, value in vocab.items()}
+
+    def calculate_pair_frequency(self, vocab):
+        pair_frequencies = defaultdict(int)
+        for word, frequency in vocab.items():
+            chars = word.split()
+            for idx in range(len(chars) - 1):
+                pair_frequencies[(chars[idx], chars[idx + 1])] += frequency
+        return pair_frequencies
 
 
 if __name__ == "__main__":
     corpus = "Tom is working hard to build a tokeniser because a tokeniser is working for tom"
     encoder = BytePairEncoding()
-    print(encoder.bin_vocab(corpus))
+    vocab = encoder.get_vocab_frequencies(corpus)
+    vocab = encoder.split_vocab_into_chars(vocab)
+    freqs = encoder.calculate_pair_frequency(vocab)
+    print(freqs)
