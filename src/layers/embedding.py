2c648b9fa3daa6362c79e7adfda55261e5d7c75a
pytergeist <tompopeworks@gmail.com>
2024-09-08 14:56:07 +0100

    Adding subclassed embedding layer with tf.gather for embedding lookup

--- src/layers/embedding.py
+++ src/layers/embedding.py
@@ -1,1 +1,34 @@
 # embedding.py
+
+import tensorflow as tf
+
+
+class EmbeddingLayer(tf.keras.layers.Layer):
+    def __init__(self, vocab_size, d_model, **kwargs):
+        super(EmbeddingLayer, self).__init__(**kwargs)
+        self.vocab_size = vocab_size
+        self.d_model = d_model
+        self.embedding_matrix = self.add_weight(
+            shape=(vocab_size, d_model),
+            initializer="random_normal",
+            trainable=True,
+            name="embedding_matrix",
+        )
+
+    def call(self, inputs):
+        return tf.gather(self.embedding_matrix, inputs)
+
+
+if __name__ == "__main__":
+    vocab_size = 1000
+    d_model = 64
+
+    embedding_layer = EmbeddingLayer(vocab_size=vocab_size, d_model=d_model)
+
+    input_sequence = tf.constant([[1, 23, 45, 67], [89, 123, 456, 789]])
+
+    output = embedding_layer(input_sequence)
+
+    print("Input Shape:", input_sequence.shape)
+    print("Embedding Output Shape:", output.shape)
+    print("Embedding Output:\n", output.numpy())
