0442b4bb43ea90164e5231f3914137c3de9f3cb5
pytergeist <tompopeworks@gmail.com>
2024-09-16 18:50:38 +0100

    Adding feed forward layer to feeed forward network

--- src/blocks/feed_forward_network.py
+++ src/blocks/feed_forward_network.py
@@ -1,4 +1,6 @@
 # feed_forward_network
+import os
+
 import tensorflow as tf
 
 from src.layers.feed_forward import FeedForwardLayer
@@ -6,12 +8,25 @@ from src.layers.feed_forward import FeedForwardLayer
 class FeedForwardNetwork(tf.keras.Model): # TODO add feed forward layer?
     def __init__(self, d_model=512, d_ff=2048, *args, **kwargs):
         super(FeedForwardNetwork, self).__init__(*args, **kwargs)
-        self.dense_1 = tf.keras.layers.Dense(units=d_ff, activation="relu")
-        self.relu_1 = tf.keras.layers.ReLU()
-        self.dense_2 = tf.keras.layers.Dense(units=d_model)
+        self.dense_1 = FeedForwardLayer(units=d_ff)
+        self.relu_1 = tf.keras.layers.ReLU() # TODO: implament ReLU activation function
+        self.dense_2 = FeedForwardLayer(units=d_model)
 
     def call(self, inputs):
         x = self.dense_1(inputs)
         x = self.relu_1(x)
         x = self.dense_2(x)
         return x
+
+
+if __name__ == "__main__":
+    tf.random.set_seed(42)
+
+    sample_input = tf.random.uniform((64, 50, 512))
+
+    ffn = FeedForwardNetwork(d_model=512, d_ff=2048)
+
+    output = ffn(sample_input)
+
+    print("Output shape:", output.shape)
+    print("Sample output:", output[0][0])
