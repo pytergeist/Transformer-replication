3484455dd5e5de5b4bc3abbf0cd80ddaa72e0e80
pytergeist <tompopeworks@gmail.com>
2024-09-18 18:28:17 +0100

    Adding ReLU activation function code

--- src/activations/relu.py
+++ src/activations/relu.py
@@ -1,1 +1,11 @@
 # relu.py
+
+import tensorflow as tf
+
+
+class ReLUActivation(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super(ReLUActivation, self).__init__(**kwargs)
+
+    def call(self, inputs):
+        return tf.math.maximum(inputs, 0)
